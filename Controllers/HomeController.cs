639c56975819edc3fde1a32eb9b5083b379fa12b
giova <giova@DESKTOP-MOK3MTB>
2025-10-07 16:52:18 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebSuportePim.Models;
+
+namespace WebSuportePim.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        private List<(string Usuario, string Senha)> usuarios = new List<(string, string)>
+        {
+            ("admin", "123"),
+            ("giovanna", "456"),
+            ("teste", "abc")
+        };
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            bool loginValido = false;
+
+            // Laço para validar usuário
+            foreach (var u in usuarios)
+            {
+                if (u.Usuario == username && u.Senha == password)
+                {
+                    loginValido = true;
+                    break;
+                }
+            }
+
+            if (loginValido)
+            {
+                return RedirectToAction("Dashboard", "Home");
+            }
+            else
+            {
+                ViewBag.Erro = "Usuário ou senha inválidos.";
+                return View();
+            }
+        } 
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebSuportePim.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
